c68ed1d6362f7d50b75b8fb38f8e931c91ddfbe0
Muhammad Kholif Arrahman <muhammad.2799@students.amikom.ac.id>
2020-07-01 19:33:15 +0700

    inisialisasi project

---
+++ tugas/Karyawan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tugas
+
+{
+    public abstract class Karyawan
+{
+    public string NIK;
+    public string Nama;
+    public abstract double Gaji();
+}
+}
